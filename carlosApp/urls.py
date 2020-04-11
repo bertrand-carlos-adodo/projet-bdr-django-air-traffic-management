5e77a9b27c529aa6e43a6498092f469242e22d63
Bertrand Carlos ADODO <carlossenameadodo@gmail.com>
2020-04-11 18:58:36 +0200

    Working progress

--- carlosApp/urls.py
+++ carlosApp/urls.py
@@ -6,5 +6,9 @@ app_name = 'trafic'
 urlpatterns = [
     path('plan/', views.plan, name = 'plan'),
     path('densite/', views.densite, name = 'densite'),
-    path('entite/', views.entite, name = 'entite'),
+    path('airline/', views.show_airline, name = 'entite'),
+    path('airport/', views.show_airport, name = 'airport'),
+    path('city/', views.show_city, name = 'city'),
+    path('country/', views.show_pays, name = 'pays'),
+    path('plane/', views.show_avion, name = 'avion'),
 ]
--- carlosApp/views.py
+++ carlosApp/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import Airline
+from .models import *
 
+@login_required
 def show_airline(request):
     	airline = Airline.objects.all()
-    	return render(request, {'airline':airline})
+    	return render(request, 'trafic/entite.html', {'airline':airline[:200],'airlines':airline})
+
+@login_required
+def show_airport(request):
+    	airport = Airport.objects.all()
+    	return render(request, 'trafic/airport.html', {'airport':airport[:200],'airports':airport})
+
+@login_required
+def show_city(request):
+    	city = City.objects.all()
+    	return render(request, 'trafic/city.html', {'city':city[:100],'cities':city})
+
+@login_required
+def show_pays(request):
+    	pays = Country.objects.all()
+    	return render(request, 'trafic/pays.html', {'pays':pays[:100],'payss':pays})
+
+@login_required
+def show_avion(request):
+    	avion = Plane.objects.all()
+    	return render(request, 'trafic/avion.html', {'avion':avion[:150],'avions':avion})
 
 @login_required
 def plan(request):
