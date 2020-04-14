9f1238c694b9ca2b3e675572b95f4083c778d262
Bertrand Carlos ADODO <carlossenameadodo@gmail.com>
2020-04-15 01:27:42 +0200

    Mise à jour du fichier de configuration

--- AirTrafficManagement/settings.py
+++ AirTrafficManagement/settings.py
@@ -80,7 +80,30 @@ WSGI_APPLICATION = 'AirTrafficManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+''' DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'projet',
+        'OPTIONS': {'options': '-c search_path=gp5'},
+        'USER':'',
+        'HOST':'savennieres.math.univ-angers.fr',
+        'PORT':'5432',
+    }
+} '''
+
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'airtrafficmanagement',
+        'OPTIONS': {'options': '-c search_path=airtrafficmanagement'},
+        'USER': 'carlos',
+        'PASSWORD': 'carlos',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+''' DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Airtraffic',
@@ -89,8 +112,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5433',
     }
-}
-
+} '''
 
 
 # Password validation
